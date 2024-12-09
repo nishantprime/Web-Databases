ba117a61af1126bd282385c08b64874c0038e302
nishantprime <nishantkumarprime@gmail.com>
2024-12-09 06:52:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from helper import mongo_client
 
 app = Flask(__name__)
@@ -15,3 +15,18 @@ def fetch_dbs(mongo_client):
     return db_collections
 
 my_dbs = fetch_dbs(mongo_client)
+
+first_db = next(iter(my_dbs))
+last_db_collection = {first_db:my_dbs[first_db][0]}
+
+@app.route('/', methods = ['GET'])
+def home():
+    global last_db_collection
+    if request.method == 'GET':
+        db = list(last_db_collection)[0]
+        collection = last_db_collection[db]
+
+        documents = mongo_client[db][collection].find()
+        documents_list = list(documents)
+        
+        return documents_list
