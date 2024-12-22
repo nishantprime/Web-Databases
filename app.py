3e6399ece2af7bed404f7bb10a93e024f7e7a7a4
nishantprime <nishantkumarprime@gmail.com>
2024-12-22 23:33:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,8 +21,8 @@ def fetch_dbs(mongo_client):
 
 db_collection_map = fetch_dbs(mongo_client)
 
-default_db = {next(iter(db_collections_map))}
-default_collection = {db_collections_map[default_db][0]}
+default_db = {next(iter(db_collection_map))}
+default_collection = {db_collection_map[default_db][0]}
 
 @app.route('/', methods = ['GET'])
 def home():
